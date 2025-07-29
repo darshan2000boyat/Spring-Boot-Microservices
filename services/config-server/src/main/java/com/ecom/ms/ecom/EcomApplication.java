236e04f4950cf700d7ba65d785c62c88447e8dda
Darshan Boyat <forstudypurpose129@gmail.com>
2025-07-29 23:44:19 +0530

    first commit

---
+++ services/config-server/src/main/java/com/ecom/ms/ecom/EcomApplication.java
@@ -0,0 +1,15 @@
+package com.ecom.ms.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class EcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApplication.class, args);
+	}
+
+}
